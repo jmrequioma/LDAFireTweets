2930c850136767b43dc84e14df592ac15c563ce3
jmrequioma <jerome.requioma@gmail.com>
2019-02-01 13:20:01 +0800

    initial clean

--- lda.py
+++ lda.py
@@ -25,16 +25,29 @@ logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=lo
 import warnings
 warnings.filterwarnings("ignore",category=DeprecationWarning)
 
-def prepare_stopwords():
-	stop_words = stopwords.words('english')
-
 def import_dataset():
 	tweets = pd.read_csv('compiled.csv', sep=';', encoding = 'ISO-8859-1')
-	print(tweets)
+	print(tweets.head)
+	return tweets
+
+def preprocess(tweets):
+	# prepare stopwords
+	stop_words = stopwords.words('english')
+
+	# removing URLS
+	tweets.text = tweets.text.apply(lambda x: re.sub(u'http\S+', u'', x))   
+
+	# removing @... 
+	tweets.text = tweets.text.apply(lambda x: re.sub(u'(\s)@\w+', u'', x))
+
+	# removing hashtags
+	tweets.text = tweets.text.apply(lambda x: re.sub(u'#', u'', x))
+	print(tweets.head())
+
 
 def main():
-	import_dataset()
-	prepare_stopwords()
+	tweets = import_dataset()
+	preprocess(tweets)
 
 if __name__ == '__main__':
 	main()
